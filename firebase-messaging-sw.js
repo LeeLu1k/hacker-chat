e2c13e4bb3a886addb45fb5ab07b11f5a596b1de
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-12-20 15:01:19 +0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "ТВОЙ_API_KEY",
+  apiKey: "AIzaSyDjxMdWwuntPGRvPgcbpWk9LjFpw25xcQc",
   projectId: "web-app-d7b7a",
   messagingSenderId: "781762321374",
   appId: "1:781762321374:web:6fb42bae922f378bd0db4f"
